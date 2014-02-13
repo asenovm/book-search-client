1cb985650f656e3ce3fdbf6333cff6c64d580249
asenovm <asenov.m@gmail.com>
2014-02-14 00:55:31 +0200

    * Facebook login

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,6 +1,22 @@
 'use strict';
 
 angular.module('bookSearchClientApp')
-  .controller('LoginController', function ($scope) {
+    .controller('LoginController', function ($scope) {
+        FB.init({
+            appId      : '352011688261066',
+            status     : true,
+            cookie     : true,
+            xfbml      : true
+        });
 
-  });
+        $scope.login = function () {
+            FB.Event.subscribe('auth.authResponseChange', function(response) {
+                if (response.status === 'connected') {
+                } else if (response.status === 'not_authorized') {
+                  FB.login();
+                } else {
+                  FB.login();
+                }
+            });
+        };
+    });
