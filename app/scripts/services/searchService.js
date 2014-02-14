815c5271cb6770109df74ada0f6e8f5729b3798d
asenovm <asenov.m@gmail.com>
2014-02-14 02:43:08 +0200

    * Making request to a search service in order to get results

--- app/scripts/services/searchService.js
+++ app/scripts/services/searchService.js
@@ -1,9 +1,19 @@
 'use strict';
 
-angular.module('bookSearchClientApp').service('SearchService', [function () {
+angular.module('bookSearchClientApp').service('SearchService', ['$http', function ($http) {
 
     this.query = function (query) {
-    
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8080/search',
+            params: {
+                'q': query
+            }
+        }).success(function (response) {
+            console.dir(response);
+        }).error(function (response) {
+            console.dir(response);
+        });
     };
 
 }]);
