a6f15fc40ff1ee06f583ebc870268092ca4f546c
asenovm <asenov.m@gmail.com>
2014-02-15 01:06:12 +0200

    * Added title and search relevance information to the results

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -3,7 +3,6 @@
 angular.module('bookSearchClientApp')
     .controller('ResultsController', ['$rootScope', '$scope', function ($rootScope, $scope) {
       
-      console.dir($scope.books);
       $scope.setActive = function(idx) {
         $scope.slides[idx].active=true;
       }
