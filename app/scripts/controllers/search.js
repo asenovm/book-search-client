e0dbb1b23ac086aa2c14f46f3ff5ac9f73635555
asenovm <asenov.m@gmail.com>
2014-02-15 10:27:02 +0200

    * Passing userId when searching for something

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('bookSearchClientApp')
-    .controller('SearchController', ['$rootScope', '$scope', '$location', 'SearchService', function ($rootScope, $scope, $location, searchService) {
+    .controller('SearchController', ['$rootScope', '$scope', '$location', 'SearchService', 'SessionService', function ($rootScope, $scope, $location, searchService, sessionService) {
 
         $scope.search = function (query, $event) {
             var $button = $($event.target);
 
             $button.button('loading');
-            searchService.query(query).success(function (response) {
+            searchService.query(query, sessionService.getLoginInfo()).success(function (response) {
                 $rootScope.books = response.books.map(function (item) {
                     item.image = 'http://assets.chitanka.info/thumb/book-cover/00/0.200.png'; 
                     return item;
--- app/scripts/services/searchService.js
+++ app/scripts/services/searchService.js
@@ -5,9 +5,9 @@ angular.module('bookSearchClientApp').service('SearchService', ['$http', functio
     var SERVER_URL = 'http://localhost:8080',
         searchQuery;
 
-    this.query = function (query) {
+    this.query = function (query, userId) {
         searchQuery = query;
-        return $http.post(SERVER_URL + '/search', { 'q': query });
+        return $http.post(SERVER_URL + '/search', { 'q': query, 'userId': userId });
     };
 
     this.markAsRelevant = function (book, userId) {
