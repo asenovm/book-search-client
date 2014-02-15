b65cf1184770adb3b117c3d5659fe66a71873fbb
asenovm <asenov.m@gmail.com>
2014-02-15 04:18:59 +0200

    * Added loading state to the search button

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -3,16 +3,21 @@
 angular.module('bookSearchClientApp')
     .controller('SearchController', ['$rootScope', '$scope', '$location', 'SearchService', function ($rootScope, $scope, $location, searchService) {
 
-        $scope.search = function (query) {
+        $scope.search = function (query, $event) {
+            var $button = $($event.target);
+
+            $button.button('loading');
             searchService.query(query).success(function (response) {
                 $rootScope.books = response.books.map(function (item) {
                     item.image = 'http://assets.chitanka.info/thumb/book-cover/00/0.200.png'; 
                     return item;
                 });
                 $location.path('/results');
+                $button.button('reset');
             }).error(function (response) {
                 $rootScope.books = [];
                 $location.path('/results');
+                $button.button('reset');
             });
         };
 
