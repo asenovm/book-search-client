505d3d68cf2aedc00cf4c61a64ad0299d6a5e6ef
asenovm <asenov.m@gmail.com>
2014-02-15 04:03:20 +0200

    * Sending the query to the relevance service

--- app/scripts/services/searchService.js
+++ app/scripts/services/searchService.js
@@ -2,14 +2,16 @@
 
 angular.module('bookSearchClientApp').service('SearchService', ['$http', function ($http) {
 
-    var SERVER_URL = 'http://localhost:8080';
+    var SERVER_URL = 'http://localhost:8080',
+        searchQuery;
 
     this.query = function (query) {
+        searchQuery = query;
         return $http.post(SERVER_URL + '/search', { 'q': query });
     };
 
     this.markAsRelevant = function (book, userId) {
-        return $http.post(SERVER_URL + '/relevant', { 'userId': userId, 'book': book.title });
+        return $http.post(SERVER_URL + '/relevant', { 'userId': userId, 'book': book.title, 'query': searchQuery });
     };
 
 }]);
